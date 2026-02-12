810f0b42eead443f51d73280797eb20dabf5f379
tmjackson97 <tmjackson97@gmail.com>
2026-02-12 14:19:49 -0500

    Add files via upload

---
+++ P1LAB1_JacksonTaylor.py
@@ -0,0 +1,7 @@
+# Taylor Jackson
+# 02/12/2026
+# P1LAB1
+# Simple python program that asks for a name and then displays it next to a welcome message. 
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Welcome to CTI-110,", first_name, last_name + "!")
